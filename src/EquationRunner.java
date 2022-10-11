ce345273718244f5ab66c258eed5930448bfe94c
BT_4N2_02 <kchen9712@bths.edu>
2022-10-11 12:27:56 -0400

    Updated Design Document, Instantiated variables and constructors in LinearEquation class.

--- src/EquationRunner.java
+++ src/EquationRunner.java
@@ -4,9 +4,12 @@ public class EquationRunner {
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
 
-        System.out.println("Enter the first x-y coordinate point: ");
+        System.out.print("Enter the first x-y coordinate point: ");
         String firstPair = s.nextLine();
-        System.out.println("Enter the second x-y coordinate point: ");
+        System.out.print("Enter the second x-y coordinate point: ");
         String secondPair = s.nextLine();
+
+        System.out.println(firstPair);
+        System.out.println(secondPair);
     }
 }
--- src/LinearEquation.java
+++ src/LinearEquation.java
@@ -1,2 +1,19 @@
 public class LinearEquation {
+    private int x1;
+    private int y1;
+    private int x2;
+    private int y2;
+    private double newX;
+    public LinearEquation(int x1, int y1, int x2, int y2)
+    {
+        this.x1 = x1;
+        this.x2 = x2;
+        this.y1 = y1;
+        this. y2 = y2;
+    }
+
+    public LinearEquation(double newX)
+    {
+        this.newX = newX;
+    }
 }
