45c4aa048ce507bb5eec788493ddfdcfbf342308
BT_4N2_02 <kchen9712@bths.edu>
2022-10-12 11:57:28 -0400

    Implemented all my methods up to calcDistance(). Successfully extracted the x and y values from the string.

--- src/EquationRunner.java
+++ src/EquationRunner.java
@@ -4,12 +4,19 @@ public class EquationRunner {
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
 
-        System.out.print("Enter the first x-y coordinate point: ");
+        System.out.print("Enter the first x-y coordinate point: ");                                                     //Asks the user for the first pair
         String firstPair = s.nextLine();
-        System.out.print("Enter the second x-y coordinate point: ");
+        System.out.print("Enter the second x-y coordinate point: ");                                                    //Asks the user for the second pair
         String secondPair = s.nextLine();
 
-        System.out.println(firstPair);
-        System.out.println(secondPair);
+        int x1 = Integer.parseInt(firstPair.substring(1, firstPair.indexOf(",")));                                      //finds the first x-value
+        int y1 = Integer.parseInt(firstPair.substring(firstPair.indexOf(",") + 1, firstPair.length() -1));              //find the first y-value
+        int x2 = Integer.parseInt(secondPair.substring(1, secondPair.indexOf(",")));                                    //finds the second x-value
+        int y2 = Integer.parseInt(secondPair.substring(secondPair.indexOf(",") + 1, firstPair.length() -1));            //finds the second y-value
+
+        LinearEquation line = new LinearEquation(x1, y1, x2, y2);
+        System.out.println(line.calcDeltaY()/line.calcDeltaX());
+        System.out.println(line.displaySIForm());
+        System.out.println(line.calcDistance());
     }
 }
--- src/LinearEquation.java
+++ src/LinearEquation.java
@@ -1,9 +1,12 @@
 public class LinearEquation {
+    //Instance Variables:
     private int x1;
     private int y1;
     private int x2;
     private int y2;
     private double newX;
+
+    //Constructors
     public LinearEquation(int x1, int y1, int x2, int y2)
     {
         this.x1 = x1;
@@ -15,5 +18,37 @@ public class LinearEquation {
     public LinearEquation(double newX)
     {
         this.newX = newX;
+    }
+
+    //Methods
+    public int calcDeltaX()
+    {
+        return x2 - x1;
+    }
+
+    public int calcDeltaY()
+    {
+        return y2-y1;
+    }
+
+    public double calcYInt()
+    {
+        return y1 - x1 * ((double)calcDeltaY() / calcDeltaX());
+    }
+
+    public String displaySIForm()
+    {
+        return "y = " + calcDeltaY() + "/" + calcDeltaX() + "x + " + (int)calcYInt();
+    }
+
+    public double calcDistance()
+    {
+        return Math.sqrt(Math.pow(calcDeltaX(), 2) + Math.pow(calcDeltaY(), 2));
+    }
+
+    public void displayString()
+    {
+        System.out.println("First Pair: " + "(" + x1 + "," + y1 + ",");
+        sout
     }
 }
